4038c9dacd8a6a53c79a78311cfcbd0545476a13
Amber-Graham <ang190045@gmail.com>
2022-08-11 14:41:50 -0500

    Add two methods to complete the caeser cipher use ascii code on method 1 use shift and alphabet for method 2

--- ceaser-cipher.rb
+++ ceaser-cipher.rb
@@ -1,25 +1,40 @@
-#create a code that will create a ceaser cipher when a string is inputted.
-#make sure only string can be input
-    #if a number or symbol is inputted, raise an error that puts "please only enter letters"
-#the alphabet needs to be loop, so that when we get to z its loops back to a 
+#when using the acsii code
 
-#a string and integer calls the ceaser cipher
-#the string is split into characters (maybe, this might not work for multiple words)
-#keep the string in downcase always
-#use ACSII code? 
-#each characters is moved forward by the number called
-#if a letter gets to z and still needs to move forward, it will loop back to the beginning of the alphabet
-#if anything other than a letter is entered, throws an error that puts "Please only enter a word. Do not use symbols or numbers."
-
-#usefull methods I think would be beneficial for me:
-    #"abc".bytes #=> [97, 98, 99]
-    #[97, 98, 99].pack('c*)  #=> "abc"
-require 'pry-byebug'; binding.pry
-
-def ceaser_cipher (string, number)
-    acsii_string = string.downcase.bytes
-    acsii_string.map! {|x| x + number}
-    acsii_string.pack('c*')
+def caeser_cipher (string, number)
+  acsii_string = string.downcase.bytes
+    encryption = acsii_string.collect! do |letter|
+      if (123...200).include?(letter + number)
+        letter = (letter - 26) + number
+        else
+          letter = letter + number
+        end
+      end
+    encryption = encryption.pack('c*')
+    encryption = encryption.gsub("(", " ")
 end
 
-ceaser_cipher("cat", 3)
+caeser_cipher("cats are cool", 8)
+
+
+#when using shift and the alphabet
+
+ALPHABET = ('a'..'z').to_a.freeze #i learned that freeze will ensure that i don't intentionally mess up my variable
+ALPHABET_SIZE = ALPHABET.size
+
+def caesar_cipher(string, shift)
+  chars = string.downcase.split('')
+  chars_string = ''
+  chars.each do |char|
+    chars_string << if ALPHABET.include? char
+                    i = ALPHABET.index(char)
+                    shifted = (i + shift) % ALPHABET_SIZE
+      
+                    ALPHABET[shifted]
+                  else
+                    char
+                  end
+                end
+  chars_string
+end
+
+caesar_cipher("dogs are better than cats", 5)
